a8a81233a813c5f2408a128c7a801e5760a780ba
pablocru <pablocru@outlook.com>
2025-06-07 21:34:51 +0200

    feat: Store basic addition functionality in a reusable function

---
+++ workshops/01-basics/functions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace functions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int result = Add(firstNumber, secondNumber);
+
+        Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {result}");
+
+        firstNumber = 7;
+        secondNumber = 9;
+        result = Add(firstNumber, secondNumber);
+
+        Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {result}");
+    }
+
+    static int Add(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+}

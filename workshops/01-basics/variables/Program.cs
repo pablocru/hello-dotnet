399f63edfb8342621aef314880b730a7d8bac47e
pablocru <pablocru@outlook.com>
2025-06-07 21:31:50 +0200

    feat: Create an integer variable and print it

---
+++ workshops/01-basics/variables/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int number = 42;
+            System.Console.WriteLine("Number: " + number);
+        }
+    }
+}

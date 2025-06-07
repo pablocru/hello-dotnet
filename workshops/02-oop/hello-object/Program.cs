c6bae3a3dc7bf86bf19be8aa9785cc25e371d2a3
pablocru <pablocru@outlook.com>
2025-06-07 22:57:01 +0200

    feat: Add implementation of Person class

---
+++ workshops/02-oop/hello-object/Program.cs
@@ -0,0 +1,20 @@
+﻿Person person = new("Pablo", new DateTime(1996, 6, 28));
+Console.WriteLine($"{person.Name} is {person.GetAge()} years old.");
+
+class Person(string name, DateTime birthDate)
+{
+    public string Name { get; } = name;
+    private DateTime BirthDate = birthDate;
+
+    // - Traditional constructor
+    // public Person(string name, DateTime birthDate)
+    // {
+    //     Name = name;
+    //     BirthDate = birthDate;
+    // }
+
+    public int GetAge()
+    {
+        return (DateTime.Now - BirthDate).Days / 365;
+    }
+}

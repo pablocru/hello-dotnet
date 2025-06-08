a4331465f1931e10a1eea5e57f67ac1f83728eda
Pablo Cru Delhom <pablocru@outlook.com>
2025-06-08 13:40:51 +0200

    Create a Solution (.sln) file (#2)
    * feat: Init .sln file
    
    * feat: Add existing projects to the solution file
    
    * feat: Add create-workshop script for automated workshop setup
    
    * feat: Remove redundant .csproj file
    
    * docs: Improve project structure and CLI commands
    
    * feat: Add bootstrap workshop section to README with usage instructions for create-workshop.sh script
    
    * docs: Update usage example with arguments of an existing workshop

---
+++ create-workshop.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+# --- Variables ---
+
+if [ "$#" -ne 4 ]; then
+  echo "Error: Invalid number of arguments."
+  echo "Usage: $0 <number> <category> <name> <type>"
+  echo "Example: $0 01 basics loops console"
+  exit 1
+fi
+
+NUMBER="$1"
+CATEGORY="$2"
+NAME="$3"
+TYPE="$4"
+
+case "$TYPE" in
+  console|classlib|webapi|xunit)
+    ;;
+  *)
+    echo "Error: Unexpected type '$TYPE'. It must be: console, classlib, webapi, xunit"
+    exit 1
+    ;;
+esac
+
+WORKSHOP_DIR="workshops/${NUMBER}-${CATEGORY}/${NAME}"
+WORKSHOP_ID="${NUMBER}-${CATEGORY}/${NAME}"
+
+# --- Functions ---
+
+create_workshop() {
+  mkdir -p "$WORKSHOP_DIR"
+  cd "$WORKSHOP_DIR"
+  dotnet new ${TYPE} --use-program-main
+  dotnet build
+  cd -
+}
+
+add_to_solution() {
+  dotnet sln add $WORKSHOP_DIR
+}
+
+# --- Program ---
+
+echo "Creating workshop ${WORKSHOP_ID}..."
+create_workshop
+echo
+
+echo "Adding ${WORKSHOP_ID} to solution file..."
+add_to_solution
+echo
+
+echo "✅ Workshop ${WORKSHOP_ID} ready!"

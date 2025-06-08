8dd83f2145bde7cab13d5a9db7881107f94b218d
pablocru <pablocru@outlook.com>
2025-06-08 17:40:55 +0200

    feat: Add JSON serialization and deserialization for Person class

--- workshops/02-oop/hello-object/Program.cs
+++ workshops/02-oop/hello-object/Program.cs
@@ -1,9 +1,20 @@
-﻿Person person = new("Pablo", new DateTime(1996, 6, 28));
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Person person = new("Pablo", new DateTime(1996, 6, 28));
 Console.WriteLine($"{person.Name} is {person.GetAge()} years old.");
+
+var serializePerson = JsonSerializer.Serialize(person);
+Console.WriteLine(serializePerson);
+
+var deserializedPerson = JsonSerializer.Deserialize<Person>(serializePerson)!;
+Console.WriteLine($"{deserializedPerson.Name} is {deserializedPerson.GetAge()} years old.");
 
 class Person(string name, DateTime birthDate)
 {
     public string Name { get; } = name;
+
+    [JsonInclude]
     private DateTime BirthDate = birthDate;
 
     // - Traditional constructor

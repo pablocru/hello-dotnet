10ca6d57fdc6fed1934e8aab5a22c785b5997cb4
pablocru <pablocru@outlook.com>
2025-06-08 23:10:53 +0200

    feat: Add anonymous object example

---
+++ workshops/02-oop/anonymous-object/Program.cs
@@ -0,0 +1,12 @@
+﻿var userList = new[]
+{
+    new { Name = "Pablo", BirthDate = new DateTime(1996, 6, 28) },
+    new { Name = "Alice", BirthDate = new DateTime(1995, 5, 15) },
+    new { Name = "Bob", BirthDate = new DateTime(1994, 4, 10) }
+};
+
+Console.WriteLine("User information:");
+foreach (var user in userList)
+{
+    Console.WriteLine($"{user.Name} was born in {user.BirthDate.ToShortDateString()}.");
+}

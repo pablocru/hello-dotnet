45f8e262e8ff5d22caa76bf1ff3e6d18bdb959ac
pablocru <pablocru@outlook.com>
2025-06-08 14:20:37 +0200

    feat: Inherit and override the GetInfo method to include job title

--- workshops/02-oop/inheritance/Program.cs
+++ workshops/02-oop/inheritance/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Employee employee = new("Pablo", new DateTime(1996, 6, 28), "Software Engineer");
 
-        Console.WriteLine($"{employee.Name} is {employee.Age} years old and works as a {employee.JobTitle}.");
+        Console.WriteLine(employee.GetInfo());
     }
 }
 
@@ -15,9 +15,13 @@ class Person(string name, DateTime birthDate)
     public string Name { get; } = name;
     private readonly DateTime _birthDate = birthDate;
     public int Age => (DateTime.Now - _birthDate).Days / 365;
+
+    public string GetInfo() => $"{Name} was born on {_birthDate.ToShortDateString()} and is {Age} years old.";
 }
 
 class Employee(string name, DateTime birthDate, string jobTitle) : Person(name, birthDate)
 {
     public string JobTitle { get; set; } = jobTitle;
+
+    public new string GetInfo() => $"{base.GetInfo()} They work as a {JobTitle}.";
 }
